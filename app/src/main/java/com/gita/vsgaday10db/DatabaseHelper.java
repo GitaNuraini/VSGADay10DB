7159c6c58615df3038086f9a4cb92eae27c14274
Gita Nuraini <42348805+GitaNuraini@users.noreply.github.com>
2020-10-12 14:13:04 +0700

    edit3

--- app/src/main/java/com/gita/vsgaday10db/DatabaseHelper.java
+++ app/src/main/java/com/gita/vsgaday10db/DatabaseHelper.java
@@ -5,6 +5,9 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.util.Log;
+
+import androidx.annotation.Nullable;
+
 import java.util.ArrayList;
 
 public class DatabaseHelper {
@@ -19,9 +22,48 @@ public class DatabaseHelper {
             + TABLE_STUDENTS + "(" + KEY_ID
             + "INTEGER," + KEY_FIRSTNAME + "TEXT );";
 
-    public DatabaseHelper(Context context){
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME,null,DATABASE_VERSION);
 
         Log.d("table", CREATE_TABLE_STUDENT);
     }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + TABLE_STUDENTS
+                + "("+KEY_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+KEY_FIRSTNAME+" TEXT);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        String sql = "DROP TABLE IF EXISTS "+ TABLE_STUDENTS;
+        db.execSQL(sql);
+        onCreate(db);
+    }
+    public long addStudentDetail(String student) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(KEY_FIRSTNAME, student);
+        long ins = db.insert(TABLE_STUDENTS,null,contentValues);
+        db.close();
+        return ins;
+    }
+    public ArrayList<String> getAllStudentsList(){
+        ArrayList<String> studentsArrayList = new ArrayList<String>();
+        String name = "";
+        String selectQuery = "SELECT * FROM " + TABLE_STUDENTS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()){
+            do {
+                name = cursor.getString(cursor.getColumnIndex(KEY_FIRSTNAME));
+                studentsArrayList.add(name);
+            } while (cursor.moveToNext());
+            Log.d("array",studentsArrayList.toString());
+        }
+        return studentsArrayList;
+    }
+
+
 }
--- app/src/main/java/com/gita/vsgaday10db/MainActivity.java
+++ app/src/main/java/com/gita/vsgaday10db/MainActivity.java
@@ -3,12 +3,55 @@ package com.gita.vsgaday10db;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button_tampil, button_simpan;
+    EditText et_nama;
+    DatabaseHelper databaseHelper;
+    TextView txt_nama;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(this);
+        txt_nama = findViewById(R.id.tvall);
+        button_simpan = findViewById(R.id.btnsimpan);
+        button_tampil = findViewById(R.id.btntampil);
+        et_nama = findViewById(R.id.etnama);
+
+        button_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(et_nama.getText())) {
+                    Toast.makeText(MainActivity.this, "Harap inputkan nama", Toast.LENGTH_SHORT).show();
+                }else {
+                    databaseHelper.addStudentDetail(et_nama.getText().toString());
+                    et_nama.setText("");
+                    Toast.makeText(MainActivity.this, "Data Tersimpan", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        button_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                arrayList = databaseHelper.getAllStudentsList();
+                txt_nama.setText("");
+                for (int i = 0; i<arrayList.size(); i++){
+                    txt_nama.setText(txt_nama.getText().toString()+", "+arrayList.get(i));
+                }
+            }
+        });
     }
 }
